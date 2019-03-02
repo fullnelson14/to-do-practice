65d760c862a02a621fa2f7232eeafd11784bb1d6
fullnelson14 <bradjnelson14@gmail.com>
2019-03-02 12:52:02 -0700

    tweak

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const App = () => {
-  return <div>Hello there.</div>;
+  return (
+    <div>
+      Hello there.
+      <p>Oh how I've loved getting back into code.....</p>
+    </div>
+  );
 };
 
 export default App;
